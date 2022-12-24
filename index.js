61bcb665949d41993210506b8f4916bfccd10a53
KsanaSal <oasalivon@gmail.com>
2022-12-24 23:08:59 +0200

    Delete comment

--- index.js
+++ index.js
@@ -26,15 +26,3 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 };
 
 invokeAction(argv);
-// invokeAction({ action: "list" });
-// invokeAction({ action: "getById", id: "1" });
-// invokeAction({
-//     action: "addContact",
-//     name: "Ksana Salivon",
-//     email: "oksana@mail.com",
-//     phone: "(933) 914-3792",
-// });
-// invokeAction({
-//     action: "removeContact",
-//     id: "7adc0e42-0f51-477f-947b-7b6d2c051601",
-// });
